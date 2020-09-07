06e9ea578163eb86d46be1f2617835fb908f17b1
aeroxu <50079470+aeroxu@users.noreply.github.com>
2020-09-07 16:12:16 -0400

    Updates

---
+++ precache-manifest.175d7950b378efa4d835863cc27261c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fc2cb6ac188c55461156d130fd1ed48",
+    "url": "/movie/index.html"
+  },
+  {
+    "revision": "02e18dd4e118e5565924",
+    "url": "/movie/static/css/main.6ba53f94.chunk.css"
+  },
+  {
+    "revision": "3479b6a2c044ad80afbf",
+    "url": "/movie/static/js/2.6018da2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie/static/js/2.6018da2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02e18dd4e118e5565924",
+    "url": "/movie/static/js/main.05e5aedc.chunk.js"
+  },
+  {
+    "revision": "ed84cc0093c15cee011f",
+    "url": "/movie/static/js/runtime-main.2bc06d1f.js"
+  },
+  {
+    "revision": "c412776699c042548bfc30e7b2fa85ca",
+    "url": "/movie/static/media/search-icon.c4127766.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie/precache-manifest.0a891b42df4386240c0d761dccbc86fb.js"
+  "/movie/precache-manifest.175d7950b378efa4d835863cc27261c8.js"
 );
 
 self.addEventListener('message', (event) => {
